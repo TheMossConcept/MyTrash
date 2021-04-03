13753eaa3cd2eda5c5faec03a1a565c35d8e0cf6
Niklas Moss <nin@envidan.dk>
2021-04-03 15:17:55 +0200

    Added the authorization button. The logic needs to be in the backend to hide the secrets properly

---
+++ components/AuthorizationButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+export default function LoginButton() {
+  const onPress = async () => {
+    console.log("Not implemented")
+  }
+
+  return (<Button title="Login" onPress={onPress} />);
+}
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import LoginButton from '../components/LoginButton';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
@@ -10,6 +11,7 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <LoginButton />
     </View>
   );
 }
