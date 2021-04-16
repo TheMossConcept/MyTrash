f348b04be787eeca4b61acb8005a5dabfa40cea1
Niklas Moss <nin@envidan.dk>
2021-04-16 20:16:03 +0200

    Introduced eslint and prettier in the backend as well with the same configuration - minus the react stuff - as the frontend

--- Backend/UserInfo/index.ts
+++ Backend/UserInfo/index.ts
@@ -1,24 +1,27 @@
 import "isomorphic-fetch";
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import {Client} from "@microsoft/microsoft-graph-client";
-import CustomAuthenticationProvider from '../CustomAuthenticationProvider'
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Client } from "@microsoft/microsoft-graph-client";
+import CustomAuthenticationProvider from "../CustomAuthenticationProvider";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
   try {
     const clientOptions = {
-      authProvider: new CustomAuthenticationProvider(req.headers)
-    }
-    const client = Client.initWithMiddleware(clientOptions)
+      authProvider: new CustomAuthenticationProvider(req.headers),
+    };
+    const client = Client.initWithMiddleware(clientOptions);
     const userDetails = await client.api("/me").get();
 
-    context.res = { 
-      body: JSON.stringify(userDetails)
-    }
+    context.res = {
+      body: JSON.stringify(userDetails),
+    };
   } catch (error) {
     context.res = {
       body: `${JSON.stringify(error)}`,
-      statusCode: '500'
-    }
+      statusCode: "500",
+    };
   }
 };
 
