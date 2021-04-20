7af00000ed5d15529337a623274a3b744510f578
Niklas Moss <nin@envidan.dk>
2021-04-20 16:05:11 +0200

    Fixed the AutocompletInput and made it more general

--- Frontend/components/ClusterCreationForm.tsx
+++ Frontend/components/ClusterCreationForm.tsx
@@ -1,17 +1,19 @@
 import axios from "axios";
-import React, { FC, useContext, useState } from "react";
-import {
-  ActivityIndicator,
-  Button,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import React, { FC, useContext, useEffect, useState } from "react";
+import { ActivityIndicator, Button, StyleSheet, View } from "react-native";
 import { AccessTokenContext } from "../navigation/TabNavigator";
 import axiosUtils from "../utils/axios";
-import UserInput, { Props as UserInputProps } from "./InputElements/UserInput";
+import AutocompleteInput, {
+  SelectableEntity,
+} from "./InputElements/AutocompleteInput";
 
 type Props = {};
+
+type UserInputProps = {
+  title?: string;
+  users: SelectableEntity[];
+  selectionState: [string, (newValue: string) => void];
+};
 
 const ClusterCreationForm: FC<Props> = () => {
   // const [loading, setLoading] = useState(false);
@@ -19,117 +21,105 @@ const ClusterCreationForm: FC<Props> = () => {
     []
   );
 
-  const [productionPartnerId, setProductionPartnerId] = useState("");
-  const [collectionAdministratorId, setCollectionAdministratorId] = useState(
-    ""
-  );
-  const [logisticsPartnerId, setLogisticsPartnerId] = useState("");
+  const productionPartnerSelectionState = useState("");
+  const collectionAdministratorSelectionState = useState("");
+  const logisticsPartnerSelectionState = useState("");
 
   const accessToken = useContext(AccessTokenContext);
 
-  if (accessToken) {
-    const getAppRolesPromise = axios.get("/GetAppRoles", {
-      params: {
-        code: "aWOynA5/NVsQKHbFKrMS5brpi5HtVZM3oaw4BEiIWDaHxAb0OdBi2Q==",
-      },
-      ...axiosUtils.getSharedAxiosConfig(accessToken),
-    });
+  // Only run data fetch once, otherwise the state update of an
+  // array will cause an infinite reload because it is a new instance
+  // each time
+  useEffect(() => {
+    if (accessToken) {
+      const getAppRolesPromise = axios.get("/GetAppRoles", {
+        params: {
+          code: "aWOynA5/NVsQKHbFKrMS5brpi5HtVZM3oaw4BEiIWDaHxAb0OdBi2Q==",
+        },
+        ...axiosUtils.getSharedAxiosConfig(accessToken),
+      });
 
-    const getUsersByRolePromise = axios.get("/GetUsersByAppRoles", {
-      params: {
-        code: "aWOynA5/NVsQKHbFKrMS5brpi5HtVZM3oaw4BEiIWDaHxAb0OdBi2Q==",
-      },
-      ...axiosUtils.getSharedAxiosConfig(accessToken),
-    });
+      const getUsersByRolePromise = axios.get("/GetUsersByAppRoles", {
+        params: {
+          code: "aWOynA5/NVsQKHbFKrMS5brpi5HtVZM3oaw4BEiIWDaHxAb0OdBi2Q==",
+        },
+        ...axiosUtils.getSharedAxiosConfig(accessToken),
+      });
 
-    Promise.all([getAppRolesPromise, getUsersByRolePromise]).then(
-      ([appRolesResult, usersByRoleResult]) => {
-        const appRoles: any[] = appRolesResult.data;
-        const usersByRole: any[] = usersByRoleResult.data;
+      Promise.all([getAppRolesPromise, getUsersByRolePromise]).then(
+        ([appRolesResult, usersByRoleResult]) => {
+          const appRoles: any[] = appRolesResult.data;
+          const usersByRole: any[] = usersByRoleResult.data;
 
-        const selectionData = appRoles.reduce<UserInputProps[]>(
-          (accumulator, appRole) => {
-            const appRoleId = appRole.id;
-            const usersForAppRole = usersByRole[appRoleId];
+          const selectionData = appRoles.reduce<UserInputProps[]>(
+            (accumulator, appRole) => {
+              const appRoleId = appRole.id;
+              const usersForAppRole = usersByRole[appRoleId];
 
-            // TODO: This can be done WAY more elegantly!!
-            if (usersForAppRole.length !== 0) {
-              if (appRole.value === "CollectionAdministrator") {
-                return [
-                  ...accumulator,
-                  {
-                    users: usersForAppRole,
-                    title: appRole.displayName,
-                    selectionState: [
-                      collectionAdministratorId,
-                      setCollectionAdministratorId,
-                    ],
-                  },
-                ];
+              // TODO: This can be done WAY more elegantly!!
+              if (usersForAppRole.length !== 0) {
+                let selectionState;
+                if (appRole.value === "CollectionAdministrator") {
+                  selectionState = collectionAdministratorSelectionState;
+                }
+                if (appRole.value === "LogisticsPartner") {
+                  selectionState = logisticsPartnerSelectionState;
+                }
+                if (appRole.value === "ProductionPartner") {
+                  selectionState = productionPartnerSelectionState;
+                }
+
+                if (selectionState) {
+                  return [
+                    ...accumulator,
+                    {
+                      users: usersForAppRole,
+                      title: appRole.displayName,
+                      selectionState,
+                    },
+                  ];
+                }
               }
-              if (appRole.value === "LogisticsPartner") {
-                return [
-                  ...accumulator,
-                  {
-                    users: usersForAppRole,
-                    title: appRole.displayName,
-                    selectionState: [logisticsPartnerId, setLogisticsPartnerId],
-                  },
-                ];
-              }
-              if (appRole.value === "ProductionPartner") {
-                return [
-                  ...accumulator,
-                  {
-                    users: usersForAppRole,
-                    title: appRole.displayName,
-                    selectionState: [
-                      productionPartnerId,
-                      setProductionPartnerId,
-                    ],
-                  },
-                ];
-              }
-            }
 
-            return accumulator;
-          },
-          []
-        );
+              // Fallback to just returning the accumulator if we haven't returned before here
+              return accumulator;
+            },
+            []
+          );
 
-        setUserSelectionData(selectionData);
-        console.log(appRoles);
-        console.log(usersByRoleResult);
-      }
-    );
-    // .finally(() => setLoading(false));
+          setUserSelectionData(selectionData);
+          console.log(appRoles);
+          console.log(usersByRoleResult);
+        }
+      );
+      // .finally(() => setLoading(false));
 
-    // setLoading(true);
+      // setLoading(true);
+    }
+  }, [accessToken]);
 
-    const onCreateCluster = () => {
-      console.log("Not implemented!");
-    };
+  const onCreateCluster = () => {
+    console.log("Not implemented!");
+  };
 
-    return (
-      <View style={styles.container}>
-        {false ? (
-          <ActivityIndicator />
-        ) : (
-          userSelectionData.map((selectionData) => (
-            <UserInput
-              /* TODO: Get a better key !! */
-              key={selectionData.title}
-              selectionState={selectionData.selectionState}
-              users={selectionData.users}
-              title={selectionData.title}
-            />
-          ))
-        )}
-        <Button title="Opret cluster" onPress={onCreateCluster} />
-      </View>
-    );
-  }
-  return <Text>Unauthorized. Please refresh the page and try again</Text>;
+  return (
+    <View style={styles.container}>
+      {false ? (
+        <ActivityIndicator />
+      ) : (
+        userSelectionData.map((selectionData) => (
+          <AutocompleteInput
+            /* TODO: Get a better key !! */
+            key={selectionData.title}
+            selectionState={selectionData.selectionState}
+            entities={selectionData.users}
+            title={selectionData.title}
+          />
+        ))
+      )}
+      <Button title="Opret cluster" onPress={onCreateCluster} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
---
+++ Frontend/components/InputElements/AutocompleteInput.tsx
@@ -0,0 +1,62 @@
+import React, { FC, useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import Autocomplete from "react-native-autocomplete-input";
+
+type SelectableEntity = {
+  id: string;
+  displayName: string;
+};
+
+export type Props = {
+  title?: string;
+  entities: SelectableEntity[];
+  selectionState: [string, (newValue: string) => void];
+};
+
+const AutocompleteInput: FC<Props> = ({ title, entities, selectionState }) => {
+  const [query, setQuery] = useState("");
+  const [, setSelectedUserId] = selectionState;
+
+  const [hideSuggestionList, setHideSuggestionList] = useState(false);
+  // Whenever we edit the query once more, show the suggestion list again
+  useEffect(() => {
+    setHideSuggestionList(
+      entities.some((entity) => entity.displayName === query)
+    );
+  }, [query, entities]);
+
+  const filteredUsers = entities.filter((entity) => {
+    const lowerCaseDisplayName = entity.displayName.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+
+    return lowerCaseDisplayName.includes(lowerCaseQuery);
+  });
+
+  // The types for react-native-autocomplete-input are faulty. It is flatListProps { renderItem } that is
+  // required not renderItem on its own
+  return (
+    <View>
+      <Text>{title}</Text>
+      <Autocomplete
+        data={filteredUsers}
+        value={query}
+        onChangeText={setQuery}
+        hideResults={hideSuggestionList}
+        flatListProps={{
+          // eslint-disable-next-line react/display-name
+          renderItem: ({ item }: { item: SelectableEntity }) => {
+            const onItemPress = () => {
+              setSelectedUserId(item.id);
+              setQuery(item.displayName);
+
+              setHideSuggestionList(true);
+            };
+            return <Text onPress={onItemPress}>{item.displayName}</Text>;
+          },
+        }}
+      />
+    </View>
+  );
+};
+
+export default AutocompleteInput;
