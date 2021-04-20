291a755c61f7464b17353597607a7c2d5a405221
Niklas Moss <nin@envidan.dk>
2021-04-20 11:06:54 +0200

    Changed the baseURL to fit what is deployed

--- Frontend/utils/axios.ts
+++ Frontend/utils/axios.ts
@@ -3,8 +3,8 @@ import { AxiosRequestConfig } from "axios";
 const getSharedAxiosConfig = (accessToken: string): AxiosRequestConfig => {
   return {
     // TODO: Fix hardcoding!!
-    // baseURL: "https://houe-plastic-recycling-windows.azurewebsites.net/api",
-    baseURL: "http://localhost:7071/api",
+    baseURL: "https://houe-plastic-recycling-windows.azurewebsites.net/api",
+    // baseURL: "http://localhost:7071/api",
     headers: {
       "access-token": accessToken,
     },
