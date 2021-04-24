2471cfd64b1ce1cd5cb073f8e89553e1e8f1285f
Niklas Moss <nin@envidan.dk>
2021-04-24 16:04:34 +0200

    Added a user id filtering for clusters

--- Backend/GetClusters/index.ts
+++ Backend/GetClusters/index.ts
@@ -2,9 +2,22 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import databaseAPI, { ClusterEntity } from "../utils/DatabaseAPI";
 
 const httpTrigger: AzureFunction = async function (
-  context: Context
+  context: Context,
+  req: HttpRequest
 ): Promise<void> {
-  const clusters = await databaseAPI.find<ClusterEntity>("cluster");
+  const participatingUserId = req.query.forUserWithId;
+  const clusters = await databaseAPI.find<ClusterEntity>(
+    "cluster",
+    participatingUserId
+      ? {
+          $or: [
+            { collectionAdministratorId: participatingUserId },
+            { logisticsPartnerId: participatingUserId },
+            { productionPartnerId: participatingUserId },
+          ],
+        }
+      : undefined
+  );
   // TODO: The frontend relies on this particular return value format. Consider
   // whether that is too hard of a coupling and we need a gateway instead, or
   // if it is acceptable for now, given the scope of the project
