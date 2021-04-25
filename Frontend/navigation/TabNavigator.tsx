a7e3afcfb8b203517528143e7bf16109fc738539
Niklas Moss <nin@envidan.dk>
2021-04-25 12:07:03 +0200

    Fixed the not found screen and the state logic surrounding that

--- Frontend/navigation/TabNavigator.tsx
+++ Frontend/navigation/TabNavigator.tsx
@@ -2,8 +2,10 @@ import jwtDecode from "jwt-decode";
 import { Ionicons } from "@expo/vector-icons";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { StackScreenProps } from "@react-navigation/stack";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 
+import { Appbar } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
 import AdministrationScreen from "../screens/AdministrationScreen";
@@ -21,22 +23,38 @@ type UserInfo = {
   roles: string[];
 };
 
-export const AccessTokenContext = React.createContext<string | undefined>(
-  undefined
-);
-
-export const UserInfoContext = React.createContext<UserInfo | undefined>(
-  undefined
-);
+export const AccessTokenContext = React.createContext<string | null>(null);
 // export const UserIdentityContext = React.createContext()
 
 type Props = StackScreenProps<RootStackParamList, "Root">;
 
-const TabNavigator: FC<Props> = ({ route }) => {
+const TabNavigator: FC<Props> = ({ navigation, route }) => {
   const { accessToken, idToken } = route.params;
 
+  // TODO: Extend this to handle refreshing the access token. Consider making it as a separate hook
+  // that handles everything related to access and refresh tokens
+  const [accessTokenState, setAccessToken] = useState<string | null>(
+    accessToken
+  );
+  const logout = () => {
+    setAccessToken(null);
+    navigation.navigate("Login");
+  };
+
+  useEffect(() => {
+    if (accessTokenState) {
+      AsyncStorage.setItem("accessToken", accessTokenState);
+    } else {
+      AsyncStorage.removeItem("accessToken");
+    }
+
+    // TODO: Consider whether we need to clear this as well. For now, I
+    // don't think so as we always get a new id token when logging in, however
+    // stale state is always a mess
+    AsyncStorage.setItem("idToken", idToken);
+  }, [accessTokenState, idToken]);
+
   const userInfo: UserInfo = jwtDecode(idToken);
-  console.log(userInfo);
   const colorScheme = useColorScheme();
 
   const showAdministrationScreen = userInfo.roles.includes(
@@ -54,80 +72,82 @@ const TabNavigator: FC<Props> = ({ route }) => {
   //  user that is logged in and get the opportunity to log out if you
   //  want to
   return (
-    <AccessTokenContext.Provider value={accessToken}>
-      <UserInfoContext.Provider value={userInfo}>
-        <Tab.Navigator
-          initialRouteName="Administration"
-          tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
-        >
-          {showAdministrationScreen && (
-            <Tab.Screen
-              name="Administration"
-              component={AdministrationScreen}
-              options={{
-                tabBarIcon: ({ color }) => (
-                  <TabBarIcon name="ios-code" color={color} />
-                ),
-              }}
-            />
-          )}
-          {showCollectionAdministrationScreen && (
-            <Tab.Screen
-              name="Indsamlingsadministration"
-              component={CollectionAdministrationScreen}
-              options={{
-                tabBarIcon: ({ color }: { color: string }) => (
-                  <TabBarIcon name="ios-code" color={color} />
-                ),
-              }}
-            />
-          )}
-          {showCollectionScreen && (
-            <Tab.Screen
-              name="Indsamling"
-              component={CollectionScreen}
-              options={{
-                tabBarIcon: ({ color }: { color: string }) => (
-                  <TabBarIcon name="ios-code" color={color} />
-                ),
-              }}
-            />
-          )}
-          {showLogisticsScreen && (
-            <Tab.Screen
-              name="Logistik"
-              component={LogisticsScreen}
-              options={{
-                tabBarIcon: ({ color }: { color: string }) => (
-                  <TabBarIcon name="ios-code" color={color} />
-                ),
-              }}
-            />
-          )}
-          {showRecipientScreen && (
-            <Tab.Screen
-              name="Modtagelse"
-              component={RecipientScreen}
-              options={{
-                tabBarIcon: ({ color }: { color: string }) => (
-                  <TabBarIcon name="ios-code" color={color} />
-                ),
-              }}
-            />
-          )}
-          {showProductionScreen && (
-            <Tab.Screen
-              name="Produktion"
-              component={ProductionScreen}
-              options={{
-                tabBarIcon: ({ color }: { color: string }) => (
-                  <TabBarIcon name="ios-code" color={color} />
-                ),
-              }}
-            />
-          )}
-        </Tab.Navigator>
-      </UserInfoContext.Provider>
+    <AccessTokenContext.Provider value={accessTokenState}>
+      <Appbar.Header>
+        <Appbar.Content title={`Velkommen ${userInfo.name}`} />
+        <Appbar.Action icon="logout" onPress={logout} />
+      </Appbar.Header>
+      <Tab.Navigator
+        initialRouteName="Administration"
+        tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+      >
+        {showAdministrationScreen && (
+          <Tab.Screen
+            name="Administration"
+            component={AdministrationScreen}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon name="ios-code" color={color} />
+              ),
+            }}
+          />
+        )}
+        {showCollectionAdministrationScreen && (
+          <Tab.Screen
+            name="Indsamlingsadministration"
+            component={CollectionAdministrationScreen}
+            options={{
+              tabBarIcon: ({ color }: { color: string }) => (
+                <TabBarIcon name="ios-code" color={color} />
+              ),
+            }}
+          />
+        )}
+        {showCollectionScreen && (
+          <Tab.Screen
+            name="Indsamling"
+            component={CollectionScreen}
+            options={{
+              tabBarIcon: ({ color }: { color: string }) => (
+                <TabBarIcon name="ios-code" color={color} />
+              ),
+            }}
+          />
+        )}
+        {showLogisticsScreen && (
+          <Tab.Screen
+            name="Logistik"
+            component={LogisticsScreen}
+            options={{
+              tabBarIcon: ({ color }: { color: string }) => (
+                <TabBarIcon name="ios-code" color={color} />
+              ),
+            }}
+          />
+        )}
+        {showRecipientScreen && (
+          <Tab.Screen
+            name="Modtagelse"
+            component={RecipientScreen}
+            options={{
+              tabBarIcon: ({ color }: { color: string }) => (
+                <TabBarIcon name="ios-code" color={color} />
+              ),
+            }}
+          />
+        )}
+        {showProductionScreen && (
+          <Tab.Screen
+            name="Produktion"
+            component={ProductionScreen}
+            options={{
+              tabBarIcon: ({ color }: { color: string }) => (
+                <TabBarIcon name="ios-code" color={color} />
+              ),
+            }}
+          />
+        )}
+      </Tab.Navigator>
     </AccessTokenContext.Provider>
   );
 };
