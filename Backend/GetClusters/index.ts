e3529ea64424cad87099a4e87591671a9ae5c0e7
Niklas Moss <nin@envidan.dk>
2021-04-25 11:32:19 +0200

    Get clusters based on your relation to it instead of just you having some relationship to it

--- Backend/GetClusters/index.ts
+++ Backend/GetClusters/index.ts
@@ -2,9 +2,22 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import databaseAPI, { ClusterEntity } from "../utils/DatabaseAPI";
 
 const httpTrigger: AzureFunction = async function (
-  context: Context
+  context: Context,
+  req: HttpRequest
 ): Promise<void> {
-  const clusters = await databaseAPI.find<ClusterEntity>("cluster");
+  const {
+    collectionAdministratorId,
+    logisticsPartnerId,
+    productionPartnerId,
+  } = req.query;
+
+  const clusters = await databaseAPI.find<ClusterEntity>("cluster", {
+    $or: [
+      { collectionAdministratorId },
+      { logisticsPartnerId },
+      { productionPartnerId },
+    ],
+  });
   // TODO: The frontend relies on this particular return value format. Consider
   // whether that is too hard of a coupling and we need a gateway instead, or
   // if it is acceptable for now, given the scope of the project
