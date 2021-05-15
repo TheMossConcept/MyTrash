40273f92c3e046bd9743ac9c745fc422bbedf0ff
Niklas Moss <nin@envidan.dk>
2021-05-15 13:23:38 +0200

    Made an initial durable function work

---
+++ Backend/CreateCollectorAndAddToClusterOrchestrator/index.ts
@@ -0,0 +1,38 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ *
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *    function app in Kudu
+ */
+
+import * as df from "durable-functions";
+
+const orchestrator = df.orchestrator(function* (context) {
+  const testUser = {
+    firstName: "Durable",
+    lastName: "test",
+    email: "durable@test.dk",
+    phoneNumber: "12345678",
+    street: "Test",
+    streetNumber: "4A",
+    city: "Test",
+    zipCode: 8600,
+    role: "Administrator",
+  };
+  const test = yield context.df.callActivity("TestActivity", testUser);
+  // Replace "Hello" with the name of your Durable Activity Function.
+  // outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
+  /*
+  outputs.push(yield context.df.callActivity("Hello", "Seattle"));
+  outputs.push(yield context.df.callActivity("Hello", "London"));
+  */
+
+  // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+  return test;
+});
+
+export default orchestrator;
---
+++ Backend/TestActivity/index.ts
@@ -0,0 +1,20 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ *
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+import { AzureFunction, Context } from "@azure/functions";
+
+const TestActivity: AzureFunction = async function (
+  context: Context
+): Promise<string> {
+  return `The user is ${JSON.stringify(context.bindings.user)}!`;
+};
+
+export default TestActivity;
