5e2ce140a28e2a2dae13f8cafa7b9d3d9d5e61b9
Niklas Moss <nin@envidan.dk>
2021-06-10 15:40:38 +0200

    Now the GetCollectors endpoint is working and just needs to be made a little bit prettier

---
+++ Backend/GetCollectors/index.ts
@@ -0,0 +1,41 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Client } from "@microsoft/microsoft-graph-client";
+import CustomAuthenticationProvider from "../utils/CustomAuthenticationProvider";
+import databaseAPI, { ClusterEntity } from "../utils/DatabaseAPI";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  // TODO: Add pagination here like on the GetClusters endpoint and all the other plural get endpoints!
+  try {
+    const customAuthProvider = new CustomAuthenticationProvider();
+    const client = Client.initWithMiddleware({
+      authProvider: customAuthProvider,
+    });
+
+    const { clusterId } = req.query;
+
+    // TODO: Consider whether it is a good idea to have two different return values
+    // depending on the request issued!
+    const {
+      collectors: idsOfCollectors,
+    } = await databaseAPI.findById<ClusterEntity>("cluster", clusterId);
+
+    const filterClause = `/users?$filter=id in ${JSON.stringify(
+      idsOfCollectors
+    ).replace('/"/g', "'")}`;
+
+    const collectors = await client.api(filterClause).get();
+
+    context.res = {
+      body: JSON.stringify(collectors),
+    };
+  } catch (e) {
+    context.res = {
+      body: JSON.stringify(e),
+    };
+  }
+};
+
+export default httpTrigger;
