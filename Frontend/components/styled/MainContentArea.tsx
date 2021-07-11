1d796fffb8e0e080e2f47a17270fc7dca7de4c5d
Niklas Moss <nin@envidan.dk>
2021-07-11 23:03:35 +0200

    Fixed the background issue on web

--- Frontend/components/styled/MainContentArea.tsx
+++ Frontend/components/styled/MainContentArea.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import {
+  View,
   ImageBackground,
   ImageBackgroundProps,
   SafeAreaView,
@@ -14,20 +15,22 @@ const MainContentArea: FC<Props> = ({
   ...imageBackgroundProps
 }) => {
   return (
-    <ImageBackground
-      source={require("../../assets/images/backgrond.png")}
-      style={[styles.imageBackground, style]}
-      {...imageBackgroundProps}
-    >
-      <SafeAreaView style={{ height: "100%" }}>{children}</SafeAreaView>
-    </ImageBackground>
+    <View style={{ height: "80%" }}>
+      <ImageBackground
+        source={require("../../assets/images/backgrond.png")}
+        style={[styles.imageBackground, style]}
+        {...imageBackgroundProps}
+      >
+        <SafeAreaView style={{ height: "100%" }}>{children}</SafeAreaView>
+      </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   imageBackground: {
     width: "100%",
-    height: "80%",
+    height: "100%",
     paddingTop: 64,
     paddingHorizontal: 49,
   },
