ce24dbe2a1b7c6c3b3b9b000e4432c89a136855e
Niklas Moss <nin@envidan.dk>
2021-08-14 23:43:00 +0200

    Revert "Introduced caching to the useQueriedData hook"
    This reverts commit fc9ed757da8fc3592315f1684215e2e0cd8af573.

--- Frontend/hooks/useQueriedData.ts
+++ Frontend/hooks/useQueriedData.ts
@@ -1,23 +1,18 @@
 import axios from "axios";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useCallback, useEffect, useState } from "react";
 import { isEqual } from "lodash";
 import useAxiosConfig from "./useAxiosConfig";
 
 function useQueriedData<T>(endpoint: string, queryParams?: Object) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [queryParamsUsed, setQueryParamsUsed] = useState<Object | undefined>(
+    // If we let this be undefined, we will never call fetchData in the case where we omit
+    // queryParams. Please note that this will still cause the issue (not calling fetchData)
+    // if, for some reason, an empty object is passed as queryParams, as queryParamsUsed and
+    // queryParams will then be equal initially.
+    {}
+  );
   const [queriedData, setQueriedData] = useState<T>();
-
-  const memorizedQueryParams: Object | undefined = useMemo(() => {
-    const previousQueryParams = queryParams;
-    if (isEqual(previousQueryParams, queryParams)) {
-      return memorizedQueryParams;
-    }
-
-    return queryParams;
-  }, [queryParams]);
-
-  const cacheKey = `${endpoint}${JSON.stringify(queryParams)})`;
 
   const sharedAxiosConfig = useAxiosConfig();
 
@@ -26,13 +21,11 @@ function useQueriedData<T>(endpoint: string, queryParams?: Object) {
 
     axios
       .get(endpoint, {
-        params: memorizedQueryParams,
+        params: queryParams,
         ...sharedAxiosConfig,
       })
       .then((axiosResult) => {
         const { data } = axiosResult;
-
-        AsyncStorage.setItem(cacheKey, JSON.stringify(data));
         setQueriedData(data);
       })
       .finally(() => {
@@ -40,23 +33,16 @@ function useQueriedData<T>(endpoint: string, queryParams?: Object) {
       });
     // TODO: Figure out a way to include queryParams without having it requery all the time
     // because it is always a new object instance that is passed along (at least for each render
-  }, [cacheKey, endpoint, memorizedQueryParams, sharedAxiosConfig]);
+  }, [endpoint, queryParams, sharedAxiosConfig]);
 
   useEffect(() => {
-    const fetchIfNecessary = async () => {
-      // Only fetch as a sideEffect if the queryParams change! (this is mostly for initial
-      // fetching and fetching when e.g. the page param changes)
-      const cachedData = await AsyncStorage.getItem(cacheKey);
-
-      if (!cachedData) {
-        fetchData();
-      } else {
-        setQueriedData(JSON.parse(cachedData) as T);
-      }
-    };
-
-    fetchIfNecessary();
-  }, [cacheKey, fetchData]);
+    // Only fetch as a sideEffect if the queryParams change! (this is mostly for initial
+    // fetching and fetching when e.g. the page param changes)
+    if (!isEqual(queryParamsUsed, queryParams)) {
+      fetchData();
+      setQueryParamsUsed(queryParams);
+    }
+  }, [fetchData, queryParams, queryParamsUsed]);
 
   return { data: queriedData, refetch: fetchData, isLoading };
 }
